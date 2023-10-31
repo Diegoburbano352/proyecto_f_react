ee3bde8e50c9b3dca87b94a6b6c395cacc3edf6a
DiegoBurbano352 <dburbanog1@miumg.edu.gt>
2023-10-30 23:43:10 -0600

    react

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "dpg-ckvbb2jamefc73erfsig-a",
+    USER: "dpg-ckvbb2jamefc73erfsig-a",
+    PASSWORD: "zUa1VjSvnBB5WKuUglFa4Aetbdr2unXV",
+    DB: "proyecto_final_tgym",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
